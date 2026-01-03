529bdf53070f9d35c332befcbf9c69b041c83e81
˚ʚ 𝒩❀𝓃𝒶𝓂𝑒 ɞ˚ <156847032+thenoname-gurl@users.noreply.github.com>
2026-01-03 22:28:44 +0100

    Implement event handling in handleEvent.js

---
+++ handles/handleEvent.js
@@ -0,0 +1,18 @@
+module.exports = (client) => {
+    client.events = async (eventFiles) => {
+        for (const file of eventFiles) {
+            const event = require(`./events/${file}`);
+            if (event.once) {
+                client.once(event.name, (...args) => event.execute(...args, client));
+                if (gconfig.debug == "1") {
+                   console.log(`Loaded once excute state event: ${event.name}`)
+                }
+            } else {
+                client.on(event.name, (...args) => event.execute(...args, client));
+                if (gconfig.debug == "1") {
+                   console.log(`Loaded normal excute state event: ${event.name}`)
+                }
+            }
+        }
+    };
+}
